dac005f98145685154060eb52cc129e199c694a5
Kewelin K <kewelin.313@gmail.com>
2020-09-18 14:14:26 +0700

    add workshop

--- App.js
+++ App.js
@@ -1,15 +1,9 @@
 import * as React from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-    </View>
-  );
-}
+import ResumeFormScreen from './screens/ResumeForm'
+import ResumeDetailScreen from './screens/ResumeDetail'
 
 const Stack = createStackNavigator();
 
@@ -17,7 +11,8 @@ function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="ResumeForm" options={{ title: 'Resume Form' }} component={ResumeFormScreen}/>
+        <Stack.Screen name="ResumeDetail" options={{ title: 'Resume Detail' }} component={ResumeDetailScreen}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
